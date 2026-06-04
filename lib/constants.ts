1750bd56b6258a36d236261b75d4ee6a01b5d3fc
Gaius Emmanuel <gaiusemmanuel12@gmail.com>
2026-06-04 11:47:17 +0100

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,59 @@
+export type EventItem = {
+    title: string;
+    image: string;
+    slug: string;
+    location: string;
+    date: string;
+    time: string;
+}
+
+export const events: EventItem[] = [
+  {
+    title: "Abuja Tech Connect 2026",
+    image: "/images/event1.png",
+    slug: "abuja-tech-connect-2026",
+    location: "International Conference Centre, Abuja",
+    date: "June 18, 2026",
+    time: "10:00 AM",
+  },
+  {
+    title: "Summer Music Festival",
+    image: "/images/event2.png",
+    slug: "summer-music-festival",
+    location: "Eko Atlantic, Lagos",
+    date: "June 21, 2026",
+    time: "6:00 PM",
+  },
+  {
+    title: "Frontend Developers Meetup",
+    image: "/images/event3.png",
+    slug: "frontend-developers-meetup",
+    location: "Civic Centre, Lagos",
+    date: "July 3, 2026",
+    time: "2:00 PM",
+  },
+  {
+    title: "Creative Design Workshop",
+    image: "/images/event4.png",
+    slug: "creative-design-workshop",
+    location: "Radisson Blu, Ikeja",
+    date: "July 10, 2026",
+    time: "9:00 AM",
+  },
+  {
+    title: "Startup Founders Summit",
+    image: "/images/event5.png",
+    slug: "startup-founders-summit",
+    location: "Transcorp Hilton, Abuja",
+    date: "July 16, 2026",
+    time: "11:00 AM",
+  },
+  {
+    title: "Afrobeats Live Experience",
+    image: "/images/event6.png",
+    slug: "afrobeats-live-experience",
+    location: "Tafawa Balewa Square, Lagos",
+    date: "July 24, 2026",
+    time: "7:30 PM",
+  },
+];
